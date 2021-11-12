79185e3773ec4fcc979ed8a99734770d10366d66
Meltos <sovikovmeltos357@gmail.com>
2021-11-12 11:37:15 +1000

    Add project files.

---
+++ NevermoreUI/Commands.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NevermoreUI
+{
+    public class Commands : ICommand
+    {
+        Action action;
+        public event EventHandler CanExecuteChanged;
+
+        public Commands(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
+
---
+++ NevermoreUI/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NevermoreUI
+{
+    public class MainViewModel
+    {
+        string url = @"http://localhost:19234/api/True";
+
+        public MainViewModel()
+
+        {
+                
+        }
+    }
+}
